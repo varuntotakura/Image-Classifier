82c9d6de5f1e6880ae0a865e3047473982509aa1
VARUN TOTAKURA <totakura.varun@gmail.com>
2020-06-01 14:04:36 +0530

    Commit

---
+++ App-MobileNetV2.py
@@ -0,0 +1,33 @@
+import keras
+from keras import backend as K
+from keras.layers.core import Dense, Activation
+from keras.optimizers import Adam
+from keras.metrics import categorical_crossentropy
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
+from keras.models import Model
+from keras.applications import imagenet_utils
+from keras.layers import Dense,GlobalAveragePooling2D
+from keras.applications import MobileNetV2
+from keras.applications.mobilenetv2 import preprocess_input
+import numpy as np
+from IPython.display import Image
+from keras.optimizers import Adam
+import PIL
+from PIL import Image
+import requests
+from io import BytesIO
+import cv2
+
+mobile = keras.applications.mobilenetv2.MobileNetV2()
+def prepare_image(file):
+    img_path = ''
+    img = image.load_img(img_path + file, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    img_array_expanded_dims = np.expand_dims(img_array, axis=0)
+    return keras.applications.mobilenet.preprocess_input(img_array_expanded_dims)
+
+preprocessed_image = prepare_image('I.jpg')
+predictions = mobile.predict(preprocessed_image)
+results = imagenet_utils.decode_predictions(predictions)
+print(results)
